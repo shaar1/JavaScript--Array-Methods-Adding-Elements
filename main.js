918f6aa9a9769197a8375e15171868e4f2d97eb2
Abdallah <abdallahchaar90@gmail.comh>
2021-01-09 17:15:32 +0100

    träning hur man kan ligga en ny namn

---
+++ main.js
@@ -0,0 +1,42 @@
+/* global console, alert, prompt*/
+/*
+var friends = [
+    "Hassan",
+    "Soha",
+    "Ahmad",
+    "Zahra",
+    "Hind" // index 4
+
+];
+
+console.log(friends);
+
+console.log(friends.length);
+
+friends[5] = "samer";
+
+console.log(friends.length);
+
+console.log(friends);
+*/
+
+var friends = [
+    "Hassan",
+    "Soha",
+    "Ahmad",
+    "Zahra",
+    "Hind" // index 4
+
+];
+
+console.log(friends);
+
+console.log(friends.length);
+
+friends[5] = "samer";
+
+console.log(friends.length);
+
+console.log(friends);
+
+
